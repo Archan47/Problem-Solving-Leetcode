dae3be07748f2c63b625b17d27b5236c73e4743c
Archan Kundu Chowdhury <chowdhuryarchan76@gmail.com>
2026-07-03 18:05:07 +0530

    1179-GAME-PLAY-ANALYSIS-I Stats: Time: 4040 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT player_id,event_date AS first_login
+FROM Activity a
+WHERE event_date =(
+SELECT MIN(event_date) FROM Activity WHERE player_id = a.player_id);
