c2b6d906106cb659a5ccfe535577d50db8401946
Archan Kundu Chowdhury <chowdhuryarchan76@gmail.com>
2026-07-10 13:41:37 +0530

    0209-MINIMUM-SIZE-SUBARRAY-SUM Stats: Time: 20 ms (15.7%), Space: 30.6 MB (41.43%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,20 +1,33 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        prefix = [0]
-        for num in nums:
-            prefix.append(prefix[-1] + num)
-        ans = float('inf')
-        n = len(nums)
-        for i in range(n):
-            need = prefix[i] + target
-            left = i + 1
-            right = n
-            while left <= right:
-                mid = (left + right) // 2
-                if prefix[mid] >= need:
-                    ans = min(ans, mid - i)
-                    right = mid - 1
-                else:
-                    left = mid + 1
-        return 0 if ans == float('inf') else ans
-        
+        currentSum = 0
+        minWindow = float('inf')
+        low = 0
+        high = 0
+        while high < len(nums):
+            currentSum += nums[high]
+            high += 1
+            while currentSum >= target:
+                currentWindowSize = high - low
+                minWindow = min(minWindow,currentWindowSize)
+                currentSum -= nums[low]
+                low += 1
+        return 0 if minWindow == float('inf') else minWindow
+
+        # prefix = [0]
+        # for num in nums:
+        #     prefix.append(prefix[-1] + num)
+        # ans = float('inf')
+        # n = len(nums)
+        # for i in range(n):
+        #     need = prefix[i] + target
+        #     left = i + 1
+        #     right = n
+        #     while left <= right:
+        #         mid = (left + right) // 2
+        #         if prefix[mid] >= need:
+        #             ans = min(ans,mid - i)
+        #             right = mid - 1
+        #         else:
+        #             left = mid + 1
+        # return 0 if ans == float('inf') else ans       
